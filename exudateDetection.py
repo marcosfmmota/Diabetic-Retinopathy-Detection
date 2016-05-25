4fde6dafeb9004e95a449349bc5f070d1d87058b
marcosfmmota <marcosfmmota@gmail.com>
2016-05-25 14:24:08 -0500

    exudates extraction

--- exudateDetection.py
+++ exudateDetection.py
@@ -11,38 +11,48 @@ def histBGRChannels(image):
     return [histB,histG,histR]
 
 #Function that given an image and threshold decides if a window is in a exudate
+
+def convertColorToBlackWhite(colorImage):
+    im_gray = cv2.cvtColor(colorImage,cv2.COLOR_BGR2GRAY)
+    (thresh,im_bw) = cv2.treshold(im_gray, 128,255 , cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    thresh = 127
+    im_bw = cv2.threshold(im_gray,thresh,255,cv2.THRESH_BINARY)[1]
+    return im_bw
+
 def exudateDetection(image,threshold,windowSize):
     row,col,chan = image.shape
-    exudate_image = np.zeros([row,col])
-
+    #exudate_image = np.zeros([row,col,3])
+    exudate_image = np.zeros([row, col])
     for r in range(0,image.shape[0] - windowSize ,windowSize):
         for c in range(0,image.shape[1] - windowSize ,windowSize):
             windowImage = image[r:r+windowSize,c:c+windowSize]
             histogram = histBGRChannels(windowImage)
-            array_of_median=[]
 
-            for i in range(len(histogram)):
-                array_of_median.append(np.median(histogram[i]))
 
-            print(array_of_median)
-            if np.greater_equal(array_of_median,threshold).all():
-                exudate_image[r:r+windowSize,c:c+windowSize] = np.ones([windowSize,windowSize])*255
-            # else:
-            #     exudate_image[r:r + windowSize, c:c + windowSize] = np.zeros([windowSize, windowSize])
+            #
+            # array_of_BGR=[]
+            #
+            # if np.greater_equal(array_of_BGR,threshold).all():
+            #     exudate_image[r:r+windowSize,c:c+windowSize] = np.ones([windowSize,windowSize])*255
+            # # else:
+            # #     exudate_image[r:r + windowSize, c:c + windowSize] = np.zeros([windowSize, windowSize])
 
-    cv2.imwrite("exudates.jpg",exudate_image)
+            lower_color = np.array(threshold)
+            upper_color = np.array([255,255,255])
+
+            exudate_mask = cv2.inRange(windowImage,lower_color,upper_color)
+            #exudate_window = cv2.bitwise_and(windowImage,windowImage,mask=exudate_mask)
+
+            exudate_image[r:r + windowSize, c:c + windowSize] = exudate_mask #exudate_window
+
+    #binaryImage = convertColorToBlackWhite(exudate_image)
+    return exudate_image
 
 
 if __name__ == "__main__":
-    image = cv2.imread("1.jpg")
-    threshold = [0,75,150]
-    windowSize = 50
-    exudateDetection(image,threshold,windowSize)
-    # hist = histRGBChannels(image)
-    # color = ('b','g','r')
-    # print(hist)
-    # for i,col in enumerate(color):
-    #     plt.plot(hist[i],color = col)
-    #     plt.xlim([0,256])
-    # plt.show()
-
+    image = cv2.imread("1008_equalized2.jpg")
+    im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold = [20,155,170]
+    windowSize = 20
+    exudate_image = exudateDetection(image,threshold,windowSize)
+    cv2.imwrite("1008_equalized2_exudates.jpg",exudate_image)
